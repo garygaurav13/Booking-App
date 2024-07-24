89732814871e4e9dc198b9c04da67fff024a21ef
garygaurav1313 <garygaurav13@gmail.com>
2024-07-24 13:57:02 +0530

    Setup Next Project

--- backend/controllers/roomControllers.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export const allRooms = async (req: NextRequest) => {
-  return NextResponse.json({
-    data: "hello",
-  });
-};
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit-v2",
-    DB_URI: "",
-  },
-};
-
-module.exports = nextConfig;
